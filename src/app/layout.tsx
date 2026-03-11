c234b577c8e01788d0df0e50762b025a806829dd
sitanshu01 <nayansitanshu@gmail.com>
2026-03-12 05:06:27 +0530

    hello frontend walo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import CustomCursor from "@/components/custom/Cursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+        <CustomCursor />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-//import Image from "next/image";
-//import styles from "./profile.module.css";
-//import EventsPage from "./events/page";
-//import ProfilePage from "./Profile/ProfilePage";
-//import Test from "./Profile/test";
-import CustomCursor from "@/components/custom/Cursor";
-import LoginPage from "./login/page";
-//import Timeline from "./timeline/Timeline";
-
 export default function Home() {
   return (
     <>
-      <CustomCursor />
-      <LoginPage />
     </>
   );
 }
